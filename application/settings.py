c7066b8d392f6328cbc89e6fcb91f3911f895b60
CooperElektrik <phamminhduc150108@gmail.com>
2025-06-23 14:17:27 +0700

    Update Fancy Light, add Fancy Dark theme

--- application/settings.py
+++ application/settings.py
@@ -17,7 +17,8 @@ LOCALIZATION_DIR = "localization"  # Directory containing .qm files
 THEME_DIR = "ui/styles"  # Directory containing .qss files
 LIGHT_THEME_FILE = "light_theme.qss"  # Specific light theme
 DARK_THEME_FILE = "dark_theme.qss"  # Specific dark theme
-FANCY_LIGHT_THEME_FILE = "fancy_light_theme.qss"  # Glassmorphism theme
+FANCY_LIGHT_THEME_FILE = "fancy_light_theme.qss"  # Fancy theme
+FANCY_DARK_THEME_FILE = "fancy_dark_theme.qss"  # Fancy theme (Dark)
 WINDOWS_95_FILE = "win95_theme.qss"  # Windows 95'ish theme
 TEMPLE_OS_FILE = "temple_os_theme.qss"  # Terry A. Davis
 SPLASH_IMAGE_FILE = os.path.join(IMAGES_DIR, "splash.jpg")  # Splash screen image
@@ -26,6 +27,7 @@ AVAILABLE_THEMES = {
     "Light": LIGHT_THEME_FILE,
     "Fancy Light": FANCY_LIGHT_THEME_FILE,
     "Dark": DARK_THEME_FILE,
+    "Fancy Dark": FANCY_DARK_THEME_FILE,
     "Windows 95": WINDOWS_95_FILE,
     "Temple OS": TEMPLE_OS_FILE,
 }  # Name to filename mapping
